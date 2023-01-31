9f372513952b480dfe572fd593ce141d450a8d78
JettCanavarro <98285768+JettCanavarro@users.noreply.github.com>
2023-01-31 09:02:38 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -9,6 +9,7 @@ node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
+node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y nginx"))
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
 node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
 
