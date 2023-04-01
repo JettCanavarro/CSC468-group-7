690d79243aa51b12f1da085ca8babb68a3505520
scoffey1 <89920392+scoffey1@users.noreply.github.com>
2023-03-31 20:28:42 -0400

    Update Dockerfile.sh

--- Database/Dockerfile.sh
+++ Database/Dockerfile.sh
@@ -1,1 +1,13 @@
+FROM mysql/mysql-server:latest
+#ight bring error if llatest version is different
 
+# Set the root password for MySQL
+ENV MYSQL_ROOT_PASSWORD=password
+
+# Create a database and user
+ENV MYSQL_DATABASE=database
+ENV MYSQL_USER=user
+ENV MYSQL_PASSWORD=123
+
+# Copy the custom SQL script into the container
+COPY ./init.sql /docker-entrypoint-initdb.d/
