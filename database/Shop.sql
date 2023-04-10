0d70790183c724ee8ce569d292b3651a74e921a8
jessarr10 <78123393+jessarr10@users.noreply.github.com>
2023-04-09 21:19:21 -0400

    Delete Shop.sql

--- database/Shop.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Shop (
-  shop_id INT PRIMARY KEY AUTO_INCREMENT,
-  city_id INT,
-  shop_name VARCHAR(100),
-  shop_address VARCHAR(200),
-  FOREIGN KEY (city_id) REFERENCES City(city_id)
-);
