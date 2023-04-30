b1e61073cad41163c9b94b5db2052516ec88a9ca
Mateen Ali <70352702+ma963495@users.noreply.github.com>
2023-04-30 12:42:01 -0400

    Update MainPage.js

--- webui/src/MainPage.js
+++ webui/src/MainPage.js
@@ -1,55 +1,57 @@
-import React, { useState } from 'react';
-import './MainPage.css';
-import logo from './img/agu logo 3.png';
-import backgroundImg from './img/background.png';
+  import React, { useState } from 'react';
+  import './MainPage.css';
+  import logo from './img/agu logo 3.png';
+  import backgroundImg from './img/background.png';
 
-function MainPage(props) {
-  const { handlePageChange } = props;
-  const backgroundStyle = {
-    backgroundImage: `url(${backgroundImg})`,
-  };
-  
-  const [showAboutUs, setShowAboutUs] = useState(false);
+  function MainPage(props) {
+    const { handlePageChange } = props;
+    const backgroundStyle = {
+      backgroundImage: `url(${backgroundImg})`,
+    };
+    
+    const [showAboutUs, setShowAboutUs] = useState(false);
 
-  const toggleAboutUs = () => {
-    setShowAboutUs(!showAboutUs);
-  };
+    const toggleAboutUs = () => {
+      setShowAboutUs(!showAboutUs);
+    };
 
-  return (
-    <div className="MainPage">
-      <div className="Header">
-        <img src={logo} alt="All Ground Up Logo" />
-        <div className="Buttons">
-          <button onClick={() => handlePageChange('SignUp')}>Sign Up</button>
-          <button onClick={() => handlePageChange('Login')}>Login</button>
-        </div>
-      </div>
-      <div className="Container" style={backgroundStyle}>
-        <div className="Container">
-          <form className="SearchBar">
-          <input type="text" placeholder="Look up the Restaurants" />
-          <button type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-      <div className="Footer">
-        <div className="FooterButtons">
-          <button className="FooterButton" onClick={toggleAboutUs}>About Us</button>
-          <button className="FooterButton">Contact Us</button>
-          <button className="FooterButton">Reviews</button>
-        </div>
-      </div>
-      {showAboutUs && 
-        <div className="AboutUsModal">
-          <div className="AboutUsContent">
-            <span className="CloseButton" onClick={toggleAboutUs}>&times;</span>
-            <p>All Ground Up is a cloud-based web application designed to allow users to order coffee delivery from their favorite local coffee shops with ease. The application supports consumers, vendors, and drivers. The customers can order from the application and the order will be sent directly to one of the drivers with the best route for delivery. Vendors can change and update their menu which will be updated directly for the customers to view. All Ground up is created using Docker containers and deployed through Kubernetes.</p>
+    return (
+      <div className="MainPage">
+        <div className="Header">
+          <img src={logo} alt="All Ground Up Logo" />
+          <div className="Buttons">
+            <button onClick={() => handlePageChange('SignUp')}>Sign Up</button>
+            <button onClick={() => handlePageChange('Login')}>Login</button>
           </div>
         </div>
-      }
-    </div>
-  );
-}
+        <div className="Container" style={backgroundStyle}>
+          <div className="SearchContainer">
+            <form className="SearchBar">
+            <input type="text" placeholder="Search your Favorite Coffee shops..." />
+            <button type="submit">Search</button>
+            </form>
+          </div>
+          <div className="Slogan">
+          <h2>Your Daily Grind, Delivered</h2>
+          </div>
+        </div>
+        <div className="Footer">
+          <div className="FooterButtons">
+            <button className="FooterButton" onClick={toggleAboutUs}>About Us</button>
+            <button className="FooterButton">Contact Us</button>
+            <button className="FooterButton">Reviews</button>
+          </div>
+        </div>
+        {showAboutUs && 
+          <div className="AboutUsModal">
+            <div className="AboutUsContent">
+              <span className="CloseButton" onClick={toggleAboutUs}>&times;</span>
+              <p>All Ground Up is a cloud-based web application designed to allow users to order coffee delivery from their favorite local coffee shops with ease. The application supports consumers, vendors, and drivers. The customers can order from the application and the order will be sent directly to one of the drivers with the best route for delivery. Vendors can change and update their menu which will be updated directly for the customers to view. All Ground up is created using Docker containers and deployed through Kubernetes.</p>
+            </div>
+          </div>
+        }
+      </div>
+    );
+  }
 
-export default MainPage;
-
+  export default MainPage;
