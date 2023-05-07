b24b6e526be6b111434acc5055816921f690f454
jessarr10 <78123393+jessarr10@users.noreply.github.com>
2023-05-07 19:37:09 -0400

    Update Login.js

--- webui/src/Login.js
+++ webui/src/Login.js
@@ -1,49 +1,85 @@
 import React, { useState } from 'react';
 import './Login.css';
+import LoginSuccess from './LoginSuccess.js';
 
-const Login = ({ closeModal }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+function Login(props) {
+  const { handlePageChange } = props;
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+  
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (username === 'john' && password === '1234') {
-      // login successful
-      closeModal();
-      alert(`Welcome, ${username}!`);
-    } else {
-      setError('Invalid username or password');
-    }
+  const handleChange = (e, property) => {
+    setFormData({
+      ...formData,
+      [property]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, password } = formData;
+    const data = {
+      username,
+      password
+    };
+  
+    fetch('http://127.0.0.1:5000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'success') {
+          console.log('Form submission successful!');
+          setIsFormSubmitted(true);
+        } else {
+          console.error('Error submitting form:', data.error);
+        }
+      })
+      .catch(error => {
+        console.error('Error submitting form:', error);
+      });  
+  };  
+
+  const handleCancel = () => {
+    handlePageChange('MainPage');
   };
 
   return (
     <div className="login-container">
-      <h2>Login</h2>
+      {isFormSubmitted ? (
+        <LoginSuccess handlePageChange={handlePageChange} />
+      ) : (
+      <>
+      <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
+        <div className="form-input">
+          <label>
+            Username:
+            <input type="text" value={formData.username} onChange={(e) => handleChange(e, 'username')} />
+          </label>
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
+        <div className="form-input">
+          <label>
+            Password:
+            <input type="text" value={formData.password} onChange={(e) => handleChange(e, 'password')} />
+          </label>
         </div>
-        {error && <div className="error-message">{error}</div>}
-        <button type="submit">Login</button>
-      </form>
-    </div>
+        <div className="form-input">
+          <button type="submit">Submit</button>
+          <button type="button" onClick={handleCancel}>Cancel</button>
+        </div>
+        </form>
+      </>
+    )}
+  </div>
   );
-};
+}
 
 export default Login;
