925433f0e2f57f3055abf7f7d6d20c509cfa023f
JettCanavarro <98285768+JettCanavarro@users.noreply.github.com>
2023-05-08 17:33:20 -0400

    Update worker.py

--- worker/worker.py
+++ worker/worker.py
@@ -1,66 +1,56 @@
 import mysql.connector as sqlconnector
 from flask import Flask, render_template, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
+
 connection = sqlconnector.connect(user='user', password='123', host='mysql-container', database='maindb')
 
-
-@app.route('/signup', methods=['GET', 'POST'])
-def create_account():
+@app.route('/api/signup', methods=['POST'])
+def signup():
     data = request.get_json()
-    firstname = data.get("firstname")
-    lastname = data.get("lastname")
-    username = data.get("username")
-    password = data.get("password")
-    email = data.get("email")
-    phone = data.get("phone")
-    address = data.get("address")
-    zipcode = data.get("zipcode")
+
+    firstname = data['firstName']
+    lastname = data['lastName']
+    username = data['username']
+    password = data['password']
+    email = data['email']
+    phone = data['phoneNumber']
+    address = data['address']
+    zipcode = data['zipcode']
 
     cursor = connection.cursor()
     cursor.execute('SELECT ZIP_ID FROM ZIP WHERE ZIP_CODE IN (%s)', (zipcode, ))
     result = cursor.fetchone()
     zip_id = result[0]
 
-    cursor.execute('INSERT INTO CUSTOMER (FIRST_NAME, LAST_NAME, USERNAME, PASSWORD, EMAIL, PHONE, CUSTOMER_ADDRESS, ZIP_ID) VALUES (%s, %s, %s,%s, %s, %s,%s,%s)', (firstname, lastname, username, password, email, phone, address, zip_id))
+    cursor.execute('INSERT INTO CUSTOMER (FIRST_NAME, LAST_NAME, USERNAME, PASS, EMAIL, PHONE, CUSTOMER_ADDRESS, ZIP_ID) VALUES (%s, %s, %s,%s, %s, %s,%s,%s)', (firstname, lastname, username, password, email, phone, address, zip_id))
     connection.commit()
     cursor.close()
 
-    return "New user added"
+    return {'status': 'success'}
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/api/login', methods=['POST'])
 def login():
     data = request.get_json()
-    username = data.get("username")
-    password = data.get("password")
+
+    username = data['username']
+    password = data['password']
 
     cursor = connection.cursor()
-    cursor.execute('SELECT PASSWORD FROM CUSTOMER WHERE USERNAME IN (%s)', (username, ))
+    cursor.execute('SELECT PASS FROM CUSTOMER WHERE USERNAME IN (%s)', (username, ))
     result = cursor.fetchone()
 
     if result:
         if result[0] == password:
-            return "Log in success"
+            return {'status': 'success'}
         else:
-            return "Incorrect password"
+            return {'status': 'failure'}
     else:
-        return "User not found."
+        return {'status': 'failure'}
 
-@app.route('/menu', methods=['GET', 'POST'])
-def menu():
-     data = request.get_json()
-     menuitem = data.get("menuitem")
-     description = data.get("description")
-     ingredients = data.get("ingredients")
-     price = data.get("price")
-     categoryid = data.get("categoryid")
-        
-     cursor = connection.cursor()
-     cursor.execute('SELECT )', (categoryid, ))
-     result = cursor.fetchone()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-    connection.close()
-
+    app.run(host='127.0.0.1', port=5000)
